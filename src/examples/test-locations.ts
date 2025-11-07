82c428839f5491624b1c7fdc67b9507ea0c50fdf
Johan Bergstrom <johan.bergstrom+geo@geoguessr.com>
2025-11-07 11:48:11 +0100

    test locations

--- src/examples/test-locations.ts
+++ src/examples/test-locations.ts
@@ -2,29 +2,79 @@ import { TestLocation } from '../types';
 
 export const testLocations: TestLocation[] = [
   {
-    name: "Location 1",
+    name: "Test 1",
     panoId: "Iu7JF_lQxq0kPaHaVupiJw",
     expectedDifficulty: 1
   },
   {
-    name: "Location 2",
+    name: "Test 2",
     panoId: "CkgOEcX22J5r4bB3mjXPoA",
     expectedDifficulty: 2
   },
   {
-    name: "Location 3",
+    name: "Test 3",
     panoId: "ft8UTxuSKhyFhI7ycTjL6g",
     expectedDifficulty: 3
   },
   {
-    name: "Location 4",
+    name: "Test 4",
     panoId: "v7EcjeQ2lD1drKzVgBr_HQ",
     expectedDifficulty: 4
   },
   {
-    name: "Location 5",
+    name: "Test 5",
     panoId: "Mf0OdaX5NePiLVylK1VkiQ",
     expectedDifficulty: 5
+  },
+  {
+    name: "Test 6",
+    panoId: "1w4tBmaZ7cxPHz3AfDBoXg",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 7",
+    panoId: "YlHLhjvj6w32vjy_xhjEww",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 8",
+    panoId: "lDcpOR6lBlybeEI2PeoQ3g",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 9",
+    panoId: "uZ67K8uLf2PUSiA-Sb-byw",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 10",
+    panoId: "aloNF2-n7dvVgZFD2EbtWg",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 11",
+    panoId: "zsgjKnQAGGppxc9a8PFgjQ",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 12",
+    panoId: "GOCJdkLwlY3Dmc1L9SXLPQ",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 13",
+    panoId: "uMnJHGL0WZJr0i2d5Tks-A",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 14",
+    panoId: "pp72ti2IVXRuuXltw2-_mw",
+    expectedDifficulty: 3
+  },
+  {
+    name: "Test 15",
+    panoId: "bFk5i931vn1ohXtb8OSY5A",
+    expectedDifficulty: 3
   }
 ];
 
